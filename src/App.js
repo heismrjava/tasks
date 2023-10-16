a7e920730ac379a17cd746f67c49107e6b6201a8
heismrjava <elesmr.java@gmail.com>
2023-10-16 15:38:37 +0100

    First url

--- src/App.js
+++ src/App.js
@@ -51,9 +51,12 @@ function App() {
   };
 
   const getTasks = () => {
-    fetch("/api/tasks")
+    fetch("http://localhost:5000/api/tasks")
       .then(response => response.json())
-      .then(data => setTasks(data));
+      .then(data => {
+        setTasks(data)
+        console.log(data);
+      });
   };
   getTasks();
 
